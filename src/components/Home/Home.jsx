17bfe603cc937895fc7f880930dec4b8b3dfaf48
JuneMinazuki <xtfrancis@gmail.com>
2025-11-28 19:49:05 +0800

    Added interactive map
    Added leaflet, react-leaflet package
    Added a map of Cyberjaya in the main page
    Added a placeholder pin

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import './Home.css';
 
+// Set marker icon in React-Leaflet
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41]
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+
 function Home() {
+  // Coordinates for the Cyberjaya
+  const position = [2.9278, 101.6419];
+
   return (
     <div className="home-container">
       {/* Top Menu Bar */}
@@ -17,7 +35,24 @@ function Home() {
 
       {/* Main Content Area */}
       <div className="main-content">
-        <p>Interactive map goes here</p>
+        <MapContainer 
+          center={position} 
+          zoom={14.5} 
+          scrollWheelZoom={true} 
+          className="leaflet-container"
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          
+          {/* Placeholder Pin */}
+          <Marker position={position}>
+            <Popup>
+              A sample bin location.
+            </Popup>
+          </Marker>
+        </MapContainer>
       </div>
     </div>
   );
