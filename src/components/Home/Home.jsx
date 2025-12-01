c7f52dc3b100b9e0c4a7295c2cb68876756a32b7
JuneMinazuki <xtfrancis@gmail.com>
2025-12-01 15:45:44 +0800

    Revert "Add navigate button in popup"
    This reverts commit 29c2ca79c9ae2fc0061ff64de596b99288bb0f3e.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -118,22 +118,15 @@ function Home({ user }) {
               position={[location.lat, location.lng]}
               icon={createPin(location.type)} 
             >
-              <Popup minWidth={300} maxWidth={500}>
+              <Popup>
                 <div style={{ textAlign: 'center' }}>
                   <h3>{location.name}</h3>
                   <p style={{ margin: 0, color: 'gray' }}>{location.type}</p>
                   <hr style={{ margin: '5px 0' }}/>
                   <small>{location.address}</small>
-                  <hr style={{ margin: '5px 0' }}/>
-                  <button
-                    className="navigate-btn"
-                    onClick={() => handleNavigate(location.lat, location.lng)}
-                    style={{ marginRight: '5px' }}
-                  >
-                    Navigate
-                  </button>
                   {user && (
                     <>
+                      <hr style={{ margin: '5px 0' }}/>
                       <button className="report-btn">
                         Report Issue
                       </button>
