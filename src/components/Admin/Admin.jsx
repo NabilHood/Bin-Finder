77bb1ebe4efed82205f66ac74d6986bc18ef37db
Niilesh25 <n11lesh.sk@gmail.com>
2025-12-08 16:52:50 +0800

    Made a admin css file for admin styling

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import './Admin.css';
 
 function Admin({ user, handleLogout }) {
   const navigate = useNavigate();
